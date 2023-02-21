ad3328904b8ca2fed21c637a4bf0537c743ba015
Melvin Kempe <k3mp314@gmail.com>
2023-02-21 11:21:46 +0100

    Removed code from the develop branch

--- src/ts/app.ts
+++
@@ -1,8 +0,0 @@
-export function init() {
-    console.log("Starting things up");
-
-    let header = document.createElement("h2");
-    header.innerHTML = "Welcome to the awesome site";
-
-    document.body.appendChild(header);
-}
